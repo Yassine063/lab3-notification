98fa4436689b65fa6b226cffd1ca997ede499309
Yassine063 <yas-elkhaddioui@hotmail.fr>
2022-02-16 20:27:42 -0500

    test10

---
+++ pwa-bootstrap/index.js
@@ -0,0 +1,77 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js');
+  console.log('registered successfully')
+};
+
+
+Toasty();
+
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+
+  // Prevent the mini-infobar from appearing on mobile
+
+  e.preventDefault();
+
+  // Stash the event so it can be triggered later.
+
+  deferredPrompt = e;
+
+  // Update UI notify the user they can install the PWA
+
+   // showInstallPromotion();
+
+  // Optionally, send analytics event that PWA install promo was shown.
+
+  console.log(`'beforeinstallprompt' event was fired.`);
+
+ 
+
+  const installAppButton = document.getElementById('installAppButton');
+  if (installAppButton) {
+    installAppButton.addEventListener('click', function () {
+      deferredPrompt.prompt();
+    })
+
+  }
+
+});
+
+var option = {
+
+  animation: true,
+
+  delay: 4000
+
+};
+
+function Toasty() {
+
+  var toastHTMLElement = document.getElementById("EpicToast");
+
+  var toastElement = new bootstrap.Toast(toastHTMLElement, option);
+
+  toastElement.show();
+
+}
+
+console.log(Notification.permission)
+
+function meNotifier() {
+  Notification.requestPermission().then(function(result) {
+    console.log("permission donnée");
+  });
+ }
+
+ function envoyerNotificationThreadUtilisateur() {
+  if (Notification.permission === 'granted') {
+      var options = {
+          body: 'Ma première notification depuis index.js',
+          requireInteraction: true
+      };
+ 
+       new Notification('Hello depuis index.js', options);
+  } else {
+      console.log("aucune notification car non permis");
+  }
+ }
---
+++ pwa-bootstrap/sw.js
@@ -0,0 +1,94 @@
+this.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('v1').then(function(cache) {
+        return cache.addAll([
+
+          '/pwa-bootstrap/',
+          '/pwa-bootstrap/index.js',
+          '/pwa-bootstrap/index.html',
+          '/pwa-bootstrap/pageHorsConnexion.html',
+          '/pwa-bootstrap/mobile2.jpg',
+          '/pwa-bootstrap/ordinateur.png',
+          '/pwa-bootstrap/tablettes.jpg',
+          '/pwa-bootstrap/icon1.png',
+          '/pwa-bootstrap/icon-192x192.png',
+          '/pwa-bootstrap/icon-256x256.png',
+          '/pwa-bootstrap/icon-384x384.png',
+          '/pwa-bootstrap/icon-512x512.png',
+          '/pwa-bootstrap/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
+          '/pwa-bootstrap/bootstrap-5.1.3-dist/css/bootstrap.min.css',
+          '/pwa-bootstrap/icons-1.7.2/font/bootstrap-icons.css'
+          
+            
+        ]);
+      })
+    );
+  });
+
+  
+ 
+ this.addEventListener('fetch', function(event) {
+  console.log("Fetching ..." + event.request.url);
+  event.respondWith(cacheOrNetwork(event.request).catch(() => fallbackVersPageHorsLigne()));
+});
+
+
+async function cacheOrNetwork(request) {
+  try {
+    return await fromCache(request);
+  } catch {
+    return await fetch(request);
+  }
+};
+
+async function fromCache(request) {
+  const cache = await caches.open('v1');
+  const matching = await cache.match(request);
+  return matching || Promise.reject('no-match');
+}
+
+function fallbackVersPageHorsLigne() {
+return caches.match("/pwa-bootstarp/pageHorsConnexion.html");
+}
+
+
+this.addEventListener('push', function (e) {
+  console.log("push recu: " + e);
+  envoyerNotification();
+});
+
+
+function envoyerNotification() {
+  if (Notification.permission === 'granted') {
+      var options = {
+          body: 'Ma première notification',
+          requireInteraction: true
+      };
+
+      this.registration.showNotification('Hello', options);
+  } else {
+      console.log("aucune notification car non permis");
+  }
+} 
+
+
+// this.addEventListener('fetch', function(event) {
+
+//   console.log("Fetching ..." + event.request.url);
+
+//   event.respondWith(caches.match(event.request).then(function(response) {
+    
+//     if (response !== undefined) {
+//       return response;
+//     } else {
+
+//       console.log("Fetching from fetch ..." + event.request.url);
+
+//       return fetch(event.request);
+//     }
+//   }));
+// });
+
+
+
+  
